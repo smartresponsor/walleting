2c7efc7333d210c6786f546ebe6b56e663620b61
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-08-23 17:55:18 -0500

    Align Walleting PHP-CS-Fixer tooling

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,12 +6,21 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-    ->in([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/config'])
-    ->exclude(['var', 'vendor', 'node_modules', 'public/build'])
-    ->notPath('reference.php')
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/migrations',
+    ])
+    ->exclude([
+        'var',
+        'vendor',
+        'node_modules',
+    ])
     ->name('*.php');
 
-return (new Config())
+$config = new Config();
+
+$config
     ->setRiskyAllowed(true)
     ->setUsingCache(true)
     ->setCacheFile(__DIR__.'/var/.php-cs-fixer.cache')
@@ -25,6 +34,28 @@ return (new Config())
             'sort_algorithm' => 'alpha',
         ],
         'single_import_per_statement' => true,
+        'no_unreachable_default_argument_value' => false,
+        'logical_operators' => false,
+        'error_suppression' => false,
+        'set_type_to_cast' => false,
+        'static_lambda' => false,
+        'psr_autoloading' => false,
+        'string_line_ending' => false,
+        'non_printable_character' => false,
+        'ereg_to_preg' => false,
+        'modern_serialization_methods' => false,
+        'native_function_invocation' => false,
+        'native_constant_invocation' => false,
+        'no_alias_functions' => false,
+        'function_to_constant' => false,
+        'no_php4_constructor' => false,
+        'ordered_traits' => false,
+        'no_homoglyph_names' => false,
+        'single_line_comment_style' => false,
         'phpdoc_to_comment' => false,
+        'no_superfluous_phpdoc_tags' => false,
+        'comment_to_phpdoc' => false,
     ])
     ->setFinder($finder);
+
+return $config;
